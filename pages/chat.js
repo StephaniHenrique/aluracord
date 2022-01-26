1d7a4e7da9931fc6c8563feac00191e42abcea94
Stephani Henrique <stephani.henrique31@gmail.com>
2022-01-26 16:00:18 -0300

    aula 03

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,191 @@
-export default function PaginaDoChat() {
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React from 'react';
+import appConfig from '../config.json';
+
+export default function ChatPage() {
+    const [mensagem, setMensagem] = React.useState('');
+    const [chatMsg, setChatMsg] = React.useState([]);
+
+    function handleNovaMsg(novaMensagem) {
+        //Objeto -> não mais string
+        const mensagem = {
+            id: chatMsg.length,
+            from: 'StephaniHenrique',
+            texto: novaMensagem,
+        }
+        //... significa percorrer os elementos do array (se não estariamos criando um array dentro do outro)
+        // Importante lembrar dos [], pois neste caso estamos trabalhando com arrays
+        setChatMsg([
+            mensagem, //Formando um array de objetos
+            ...chatMsg //As novas mensagens devem ser as mais recentes no chat e portanto as primeiras a serem acessadas no array
+        ])
+        setMensagem('');
+    }
+
     return (
-        <h1>AAAAA</h1>
-    );
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.primary[500],
+                backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: appConfig.theme.colors.neutrals['000']
+            }}
+        >
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: appConfig.theme.colors.neutrals[700],
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+                }}
+            >
+                <Header />
+                <Box
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[600],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+                    <MessageList msgs={chatMsg} />
+                    {/*Foreach -> laço de repetição para percorrer os valores do array, mas não retorna valor
+                    podemos usar map */}
+                    {//chatMsg.map((msgAtual) => { //msgAtual funciona como array[i]
+                        //   return <li key={msgAtual.id}>{msgAtual.texto}</li>
+                        //})
+                    }
+
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            value={mensagem}
+                            /*arrow function -> função enxugada = function () {} == () => {}*/
+                            onChange={(event) => {
+                                setMensagem(event.target.value);
+                            }}
+                            onKeyPress={(event) => {
+                                if (event.key == 'Enter') {
+                                    event.preventDefault(); //Não ira executar sua função primária
+                                    handleNovaMsg(mensagem);
+                                }
+                            }}
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+function Header() {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflowY: 'scroll',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+
+            {props.msgs.map((mensagemAtual) => {
+                return (
+                    <Text
+                        key={mensagemAtual.id}
+                        tag="li"
+                        styleSheet={{
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                            }
+                        }}
+                    >
+                        <Box
+                            styleSheet={{
+                                marginBottom: '8px',
+                            }}
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '20px',
+                                    height: '20px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/${mensagemAtual.from}.png`}
+                            />
+                            <Text tag="strong">
+                                {mensagemAtual.from}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: appConfig.theme.colors.neutrals[300],
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                        </Box>
+                        {mensagemAtual.texto}
+                    </Text>
+                );
+            })}
+
+        </Box>
+    )
 }
